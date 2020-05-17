eea6ed86212b5529a828c4f5b78882a54138d45d
IKedi <36607876+IKedi@users.noreply.github.com>
2020-05-17 04:13:04 +0300

    Update test.lua

--- app/scripts/sgd/lib/test.lua
+++ app/scripts/sgd/lib/test.lua
@@ -1,16 +1,1 @@
 print'a'
-return
-
-local m_sb = game.Players.LocalPlayer.PlayerGui.Fakt_SGD.Drag.Main.Body.Start_butt
-local a = false
-
-while wait() do
-	if a then return;end
-	print'dont mine at night'
-end
-
-m_sb.Changed:Connect(function()
-	if m_sb.Text == "Start" then
-		a = true
-	end
-end
