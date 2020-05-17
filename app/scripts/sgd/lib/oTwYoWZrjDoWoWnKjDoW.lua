06c731a8f60d4a887a2377463343abd3ae2a6e04
IKedi <36607876+IKedi@users.noreply.github.com>
2020-05-17 04:24:56 +0300

    Update oTwYoWZrjDoWoWnKjDoW.lua

--- app/scripts/sgd/lib/oTwYoWZrjDoWoWnKjDoW.lua
+++ app/scripts/sgd/lib/oTwYoWZrjDoWoWnKjDoW.lua
@@ -1,10 +1,12 @@
 local a=game.Players.LocalPlayer.PlayerGui:WaitForChild('Fakt_SGD').Drag.Main.Body.Start_butt;local b=false
 while wait(0.5) do
-	if b then return;end
+	if b then return;print'biggergay';end
 	print'luggi'
 end
 a.Changed:Connect(function()
+	wait()
 	if a.Text == "Start" then
 		b = true
+			print'gay'
 	end
 end)
