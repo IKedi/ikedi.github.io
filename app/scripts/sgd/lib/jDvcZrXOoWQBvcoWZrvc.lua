8259dc29f3ef47009243ff3f32e230085c48a18b
IKedi <36607876+IKedi@users.noreply.github.com>
2020-05-23 06:53:09 +0300

    Update jDvcZrXOoWQBvcoWZrvc.lua

--- app/scripts/sgd/lib/jDvcZrXOoWQBvcoWZrvc.lua
+++ app/scripts/sgd/lib/jDvcZrXOoWQBvcoWZrvc.lua
@@ -5,6 +5,6 @@ while wait() do
 		game:GetService("ReplicatedStorage").DrinksGives.Give18:FireServer()
 	end
 	if _G.sgd_autosell then
-		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-4920.73242, 3766.93262, -1055.36194))
+		game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-4920.07861, 3764.83008, -1057.08215, -0.137502, -5.09118259e-08, 0.990501463, -7.72833104e-08, 1, 4.06715337e-08, -0.990501463, -7.09568155e-08, -0.137502))
 	end
 end
