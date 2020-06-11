5e23385175b369b55c8230303dea71a13d54852d
IKedi <36607876+IKedi@users.noreply.github.com>
2020-06-12 00:18:33 +0300

    Update oTwYoWZrjDoWoWnKjDoW.lua

--- app/scripts/sgd/lib/oTwYoWZrjDoWoWnKjDoW.lua
+++ app/scripts/sgd/lib/oTwYoWZrjDoWoWnKjDoW.lua
@@ -1,6 +1,9 @@
 --|Multiplier:gay|,|AutoSell:true|,|By:ur mom|--
   
-  while wait(1) do
+print'Gay script by ur mom has executed'
+print(_G.sgd_autosell)
+
+ while wait(1) do
 	if _G.sgd_kill then return end
 	print'ur gay'
 end
