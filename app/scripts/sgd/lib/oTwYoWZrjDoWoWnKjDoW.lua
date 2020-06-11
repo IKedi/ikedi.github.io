edd8bee9235fe04228323a972f4ad75961a2db82
IKedi <36607876+IKedi@users.noreply.github.com>
2020-06-12 01:30:07 +0300

    Delete oTwYoWZrjDoWoWnKjDoW.lua

--- app/scripts/sgd/lib/oTwYoWZrjDoWoWnKjDoW.lua
+++
@@ -1,9 +0,0 @@
---|Multiplier:gay|,|AutoSell:true|,|By:ur mom|--
-  
-print'Gay script by ur mom has executed'
-print(_G.sgd_autosell)
-
- while wait(1) do
-	if _G.sgd_kill then return end
-	print'ur gay'
-end
