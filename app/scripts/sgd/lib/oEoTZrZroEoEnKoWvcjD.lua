7d7aa840b8b236b6692ee4ad673a56c5722ac426
IKedi <36607876+IKedi@users.noreply.github.com>
2020-06-19 06:20:07 +0300

    Update oEoTZrZroEoEnKoWvcjD.lua

--- app/scripts/sgd/lib/oEoTZrZroEoEnKoWvcjD.lua
+++ app/scripts/sgd/lib/oEoTZrZroEoEnKoWvcjD.lua
@@ -1,4 +1,4 @@
---|Multiplier:true@Auto sell (true/false)|,|AutoSell:use syn_mouse1click()|,|By:IKedi|
+--|Multiplier:doesn't work in this game.|,|AutoSell:use syn_mouse1click()|,|By:IKedi|
 
 local st = game.Players.LocalPlayer.PlayerGui.Bin.UIs.CurrencyUi.Strength.Num
 local hrp = game.Players.LocalPlayer.Character.HumanoidRootPart
@@ -14,9 +14,9 @@ while wait() do
     
     if syn and _G.sgd_autosell then
         if _g then syn_mouse1click() end
-        if _G.multiplier == 'true' and not _g then
-            game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer("SellAll")
-        end
+        --[[if _G.multiplier == 'true' and not _g then
+            game:GetService("ReplicatedStorage").RemoteFunction:InvokeServer("SellAll") --remote doesn't work and im too lazy to fix it
+        end]]
     end
     
     for i, o in ipairs(workspace.FishCoins:GetChildren()) do
