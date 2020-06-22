204949b551695993f189fab7a90adb5d1ebd2d13
IKedi <36607876+IKedi@users.noreply.github.com>
2020-06-22 05:56:58 +0300

    Update jDoWZrXOvcZrjDQBjDvc.lua

--- app/scripts/sgd/lib/jDoWZrXOvcZrjDQBjDvc.lua
+++ app/scripts/sgd/lib/jDoWZrXOvcZrjDQBjDvc.lua
@@ -1,4 +1,4 @@
---Multiplier:50,AutoSell:false,By:IKedi--
+--Multiplier:30,AutoSell:Gems,By:IKedi--
 local rem = game:GetService("ReplicatedStorage").Events.SendTexts
 while wait() do
 	if _G.sgd_kill then return end
@@ -7,5 +7,12 @@ while wait() do
 		rem:FireServer("Game")
 		rem:FireServer("Tablet")
 		rem:FireServer("Computer")
+	end
+	if _G.sgd_autosell then
+   	    for i, o in ipairs(workspace.Gems:GetChildren()) do
+       		o.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
+       		wait(0.1)
+       		o:Destroy()
+	    end
 	end
 end
