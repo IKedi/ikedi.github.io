c0c13ea524402c7e45735aef9b1324457613ca97
IKedi <IKedi@noreply.github.com>
2020-06-22 20:26:33 +0300

    Removed scripts

--- app/scripts/sgd/lib/QBvcoWwYZroTZrQBZroT.lua
+++
@@ -1,6 +0,0 @@
---Multiplier:doesn't work in this game.,AutoSell:false,By:IKedi--
-local rem = game.Players.LocalPlayer.ninjaEvent
-while wait() do
-	if _G.sgd_kill then return end
-	rem:FireServer("swingKatana")
-end
--- app/scripts/sgd/lib/oEoEoWZrnKQBQBvcoWoW.lua
+++
@@ -1,8 +0,0 @@
---Multiplier:5,AutoSell:false,By:IKedi--
-local rem = game.ReplicatedStorage.Swing
-while wait() do
-	if _G.sgd_kill then return end
-	for i = 1, tonumber(_G.multiplier) do
-		rem:FireServer(100000)
-	end
-end
